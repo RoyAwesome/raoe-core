7356f1bf3d46cf1d1de1b963facd5ce0b61a6e11
Garrett Fleenor <kylegar@kylegar.net>
2024-06-21 19:33:58 -0700

    Update Copyright

--- core/include/core/enum.hpp
+++ core/include/core/enum.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Roy Awesome's Open Engine (RAOE)
+Copyright 2022-2024 Roy Awesome's Open Engine (RAOE)
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
