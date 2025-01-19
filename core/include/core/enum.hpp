785e08004683c02d19750eb195e31b7f33c6c33b
Garrett <kylegar@kylegar.net>
2025-01-19 14:59:05 -0800

    require RAOE_CORE_ENUM_FLAGS() macro for each enum to participate in flag utilities

--- core/include/core/enum.hpp
+++ core/include/core/enum.hpp
@@ -17,93 +17,109 @@ Copyright 2022-2024 Roy Awesome's Open Engine (RAOE)
 
 #include <type_traits>
 
-namespace raoe::core
+namespace raoe
 {
     template <typename Enum>
     concept is_integral_enum = std::is_enum<Enum>::value && std::is_integral<std::underlying_type_t<Enum>>::value;
+
+    template <is_integral_enum E>
+    struct enum_traits
+    {
+        static constexpr bool is_bitmask = false;
+    };
+
+    template <typename E>
+    concept is_bitmask_enum = is_integral_enum<E> && enum_traits<E>::is_bitmask;
 }
 
-template <raoe::core::is_integral_enum E>
+#define RAOE_CORE_FLAGS_ENUM(Enum)                                                                                     \
+    template <>                                                                                                        \
+    struct raoe::enum_traits<Enum>                                                                                     \
+    {                                                                                                                  \
+        static constexpr bool is_bitmask = true;                                                                       \
+    };
+
+template <raoe::is_integral_enum E>
 constexpr inline auto underlying(E e)
 {
     return static_cast<std::underlying_type_t<E>>(e);
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E operator~(E rhs) noexcept
 {
     return static_cast<E>(~underlying(rhs));
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E operator|(E lhs, E rhs) noexcept
 {
     return static_cast<E>(underlying(lhs) | underlying(rhs));
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E operator&(E lhs, E rhs) noexcept
 {
     return static_cast<E>(underlying(lhs) & underlying(rhs));
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E operator^(E lhs, E rhs) noexcept
 {
     return static_cast<E>(underlying(lhs) ^ underlying(rhs));
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E& operator&=(E& lhs, E rhs) noexcept
 {
     return lhs = (lhs & rhs);
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E& operator|=(E& lhs, E rhs) noexcept
 {
     return lhs = (lhs | rhs);
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_bitmask_enum E>
 constexpr E& operator^=(E& lhs, E rhs) noexcept
 {
     return lhs = (lhs ^ rhs);
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_integral_enum E>
 constexpr bool operator==(E lhs, E rhs) noexcept
 {
     return underlying(lhs) == underlying(rhs);
 }
 
-template <raoe::core::is_integral_enum E>
+template <raoe::is_integral_enum E>
 constexpr bool operator!=(E lhs, E rhs) noexcept
 {
     return underlying(lhs) != underlying(rhs);
 }
 
-namespace raoe::core
+namespace raoe
 {
-    template <is_integral_enum Enum>
+    template <is_bitmask_enum Enum>
     constexpr bool has_all_flags(Enum flags, Enum contains)
     {
         return (flags & contains) == contains;
     }
 
-    template <is_integral_enum Enum>
+    template <is_bitmask_enum Enum>
     constexpr bool has_any_flags(Enum flags, Enum contains)
     {
         return (flags & contains) != (Enum)0;
     }
 
-    template <is_integral_enum Enum>
+    template <is_bitmask_enum Enum>
     constexpr bool add_flags(Enum& flags, Enum flags_to_add)
     {
         return flags |= flags_to_add;
     }
 
-    template <is_integral_enum Enum>
+    template <is_bitmask_enum Enum>
     constexpr bool remove_flags(Enum& flags, Enum flags_to_remove)
     {
         return flags &= ~flags_to_remove;
